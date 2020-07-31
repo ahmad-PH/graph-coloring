8022ba67d3eb51a8c80aece35841b4ccc55da19f
Ahmad Pourihosseini <seyedahmadpourihosseini@gmail.com>
2020-08-01 02:01:24 +0430

    Remove my own Erdos_Renyi implementation

--- test.py
+++ test.py
@@ -2,7 +2,6 @@ import unittest
 import math
 
 from main import *
-from graph_generators import *
 from utility import combinations
 
 class TestVertexOrderHeuristic(unittest.TestCase):
@@ -16,18 +15,3 @@ class TestVertexOrderHeuristic(unittest.TestCase):
         ]
 
         self.assertEqual(vertex_order_heuristic(adj_list), [1, 0, 3, 4, 2])
-
-
-class TestErdosRenyiGenerator(unittest.TestCase):
-    def setUp(self):
-        np.random.seed(0)
-
-    def test_probability(self):
-        p = 0.3
-        possible_edges = actual_edges = 0
-        for _ in range(1000):
-            n = np.random.randint(3, 30)
-            adj_matrix = erdos_renyi(n, p)
-            actual_edges += np.sum(adj_matrix) / 2
-            possible_edges += combinations(n, 2)
-        self.assertAlmostEqual(actual_edges / possible_edges, p, places=2)
