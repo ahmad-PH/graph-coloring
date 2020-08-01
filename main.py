67f2c354f967711c3ce4991edb5ce66cfdc0d7f5
Ahmad Pourihosseini <seyedahmadpourihosseini@gmail.com>
2020-08-01 23:21:43 +0430

    Add Dataset and Dataloader, and generators for ER and WS

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
 import numpy as np
+import networkx as nx
+from matplotlib import pyplot as plt
+import os
+from torch.utils.data import Dataset, DataLoader
 
 a = [
     [1, 3],
@@ -42,7 +46,57 @@ def vertex_order_heuristic(adj_list):
 
     return result
 
+def generate_erdos_renyi(foldername, n_train, n_valid, n_test):
+    os.mkdir(foldername)
+    for subfoldername, graph_count in [('train', n_train), ('valid', n_valid), ('test', n_test)]:
+        os.mkdir(os.path.join(foldername, subfoldername))
+        for i in range(graph_count):
+            G = nx.erdos_renyi_graph(10, 0.5)
+            nx.write_adjlist(G, "{}/{}/{}.adjlist".format(foldername, subfoldername, i))
+
+def generate_watts_strogatz(foldername, n_train, n_valid, n_test):
+    os.mkdir(foldername)
+    for subfoldername, graph_count in [('train', n_train), ('valid', n_valid), ('test', n_test)]:
+        os.mkdir(os.path.join(foldername, subfoldername))
+        for i in range(graph_count):
+            G = nx.watts_strogatz_graph(10, 4, 0.5)
+            nx.write_adjlist(G, "{}/{}/{}.adjlist".format(foldername, subfoldername, i))
+
+class GraphDataset(Dataset):
+    def __init__(self, foldername):
+        file_names = os.listdir(foldername)
+        counter = 0 
+        while "{}.adjlist".format(counter) in file_names:
+            counter += 1
+        self.len = counter
+        self.foldername = foldername
+
+    def __getitem__(self, i):
+        return nx.read_adjlist('{}/{}.adjlist'.format(self.foldername, i), nodetype=int)
+
+    def __len__(self):
+        return self.len
+
+class GraphDatasetEager(Dataset):
+    def __init__(self, foldername):
+        file_names = os.listdir(foldername)
+        counter = 0 
+        while "{}.adjlist".format(counter) in file_names:
+            counter += 1
+        self.len = counter
+        self.foldername = foldername
+
+        self.graphs = [nx.read_adjlist('{}/{}.adjlist'.format(foldername, i), nodetype=int) for i in range(self.len)]
+        print(self.len)
+
+    def __getitem__(self, i):
+        return self.graphs[i]
+
+    def __len__(self):
+        return self.len
+
+
 if __name__=='__main__':
-    order = vertex_order_heuristic(a)
-    print(order)
+    generate_erdos_renyi('erdos_renyi', 1000, 200, 200)
+    generate_watts_strogatz('watts_strogatz', 1000, 200, 200)
 
