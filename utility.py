c3cc24fdc26b34b491c954d24e05f14e57da4c9e
Ahmad Pourihosseini <seyedahmadpourihosseini@gmail.com>
2020-08-02 17:02:59 +0430

    Add commented-out code for using heaps in heuristics

--- utility.py
+++ utility.py
@@ -1,6 +1,39 @@
 import math
+import heapq
 
 def combinations(n, k):
     if n < k:
         raise ValueError('n cannot be smaller than k in combination(n, k)')
     return int(math.factorial(n) / (math.factorial(n-k) * math.factorial(k)))
+
+
+class ComparableContainer:
+    def __init__(self, item, key):
+        self.item = item
+        self.key = key
+
+    def __lt__(self, other):
+        return self.key(self.item) < self.key(other.item)
+
+    def __le__(self, other):
+        return self.key(self.item) <= self.key(other.item)
+
+    def __gt__(self, other):
+        return self.key(self.item) > self.key(other.item)
+
+    def __ge__(self, other):
+        return self.key(self.item) >= self.key(other.item)
+
+    def __eq__(self, other):
+        return self.key(self.item) == self.key(other.item)
+
+    def __str__(self):
+        return str(self.item)
+
+class MinHeap:
+    def __init__(self, _list, key):
+        self.heap = [ComparableContainer(item, key) for item in _list]
+        self.heap = heapq.heapify(self.heap)
+        self.key = key
+
+    
