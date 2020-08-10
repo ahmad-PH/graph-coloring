ec00e89005f4288c960a5f28191aae7e0c872752
Ahmad Pourihosseini <seyedahmadpourihosseini@gmail.com>
2020-08-11 02:44:28 +0430

    Add tests for the singular implementation of GAT

--- test.py
+++ test.py
@@ -1,8 +1,9 @@
 import unittest
 import math
 
-from utility import combinations
 from heuristics import *
+from GAT import GraphAttentionLayer, GraphSingularAttentionLayer
+import torch
 
 graph1 =  [
     [1, 3],
@@ -56,3 +57,53 @@ class TestColorizeUsingHeuristic(unittest.TestCase):
         colors, n_colors = colorize_using_heuristic(graph2, highest_colored_neighbor_heuristic)
         self.assertListEqual(list(colors), [0, 1, 1, 0, 1])
         self.assertEqual(n_colors, 2)
+
+
+
+class TestGSATImplementation(unittest.TestCase):
+    def test_small_embedding(self):
+        # the torch.manual_seed call is to ensure they both initialize parameters the same way.
+        torch.manual_seed(0)
+        GAT = GraphAttentionLayer(10, 10, 0, 0.1)
+
+        torch.manual_seed(0)
+        GSAT = GraphSingularAttentionLayer(10, 10, 0, 0.1)
+
+        test_embeddings = torch.randn(4, 10)
+
+        adj_matrix = torch.tensor([
+            [1,1,1,0],
+            [1,1,0,1],
+            [1,0,1,0],
+            [0,1,0,1],
+        ])
+
+        gat_result = GAT.forward(test_embeddings, adj_matrix)
+        gsat_result = GSAT.forward(test_embeddings[[0,1,2]], 0)
+
+        self.assertTrue(torch.allclose(gat_result[0], gsat_result))
+
+    def test_large_embedding(self):
+        torch.manual_seed(0)
+        GAT = GraphAttentionLayer(100, 100, 0, 0.1)
+
+        torch.manual_seed(0)
+        GSAT = GraphSingularAttentionLayer(100, 100, 0, 0.1)
+
+        test_embeddings = torch.randn(4, 100)
+
+        adj_matrix = torch.tensor([
+            [1,1,1,0],
+            [1,1,0,1],
+            [1,0,1,0],
+            [0,1,0,1],
+        ])
+
+        gat_result = GAT.forward(test_embeddings, adj_matrix)
+        gsat_result = GSAT.forward(test_embeddings[[0,1,2]], 0)
+
+        self.assertTrue(torch.allclose(gat_result[0], gsat_result, rtol=1e-4))
+
+
+
+
