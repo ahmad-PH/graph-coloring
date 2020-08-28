e32f2f410f0d01d4674655ac95191f9c745d4869
Ahmad Pourihosseini <seyedahmadpourihosseini@gmail.com>
2020-08-28 15:26:27 +0430

    Add tests for ColorClassifier

--- test.py
+++ test.py
@@ -3,7 +3,10 @@ import math
 
 from heuristics import *
 from GAT import GraphAttentionLayer, GraphSingularAttentionLayer
+from graph_colorizer import ColorClassifier
 import torch
+import torch.nn as nn
+import torch.nn.functional as F
 
 graph1 =  [
     [1, 3],
@@ -103,6 +106,98 @@ class TestGSATImplementation(unittest.TestCase):
         gsat_result = GSAT.forward(test_embeddings[[0,1,2]], 0)
 
         self.assertTrue(torch.allclose(gat_result[0], gsat_result, rtol=1e-4))
+
+class TestColorClassifier(unittest.TestCase):
+    def setUp(self):
+        self.device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
+        torch.manual_seed(0)
+
+    def _create_sample_data(self, embedding_size, n_possible_colors, sample_size = 10):
+        layer1 = nn.Linear(embedding_size, 200).to(self.device)
+        layer2 = nn.Linear(200, n_possible_colors + 1).to(self.device)
+        nn.init.uniform_(layer1.weight.data, -1, 1)
+        nn.init.uniform_(layer2.weight.data, -1, 1)
+
+        with torch.no_grad():
+            x = torch.randn(sample_size, embedding_size).to(self.device)
+            y = F.softmax(layer2(torch.tanh(layer1(x))), dim = 1).to(self.device)
+
+        return x, y
+
+    def test_with_batch(self):
+        embedding_size = 200
+        n_possible_colors = 50
+        x, y = self._create_sample_data(embedding_size, n_possible_colors)
+        classifier = ColorClassifier(embedding_size, n_possible_colors, run_assertions=False).to(self.device)
+
+        optimizer = torch.optim.Adam(classifier.parameters())
+        loss_function = nn.MSELoss()
+
+        for i in range(200):
+            optimizer.zero_grad()
+            y_hat = classifier(x, n_possible_colors).to(self.device)
+            loss = loss_function(y_hat, y)
+            loss.backward()
+            optimizer.step()
+        
+        self.assertLess(loss.item(), 0.0005)
+
+    def test_without_batch(self):
+        embedding_size = 30
+        n_possible_colors = 5
+        x, y = self._create_sample_data(embedding_size, n_possible_colors, 5)
+        classifier = ColorClassifier(embedding_size, n_possible_colors, run_assertions=False).to(self.device)
+
+        optimizer = torch.optim.Adam(classifier.parameters())
+        loss_function = nn.MSELoss()
+
+        for i in range(200):
+            for j in range(x.shape[0]):
+                optimizer.zero_grad()
+                y_hat = classifier(x[j], n_possible_colors).to(self.device)
+                loss = loss_function(y_hat, y[j])
+                loss.backward()
+                optimizer.step()
+        
+        self.assertLess(loss.item(), 0.0005)
+
+    def test_n_used_colors_masks_propery(self):
+        embedding_size = 100
+        n_possible_colors = 20
+        x, y = self._create_sample_data(embedding_size, n_possible_colors)
+        classifier = ColorClassifier(embedding_size, n_possible_colors, run_assertions=False).to(self.device)
+
+        y_hat = classifier(x[0], 10)
+        self.assertEqual(torch.sum(y_hat[10:20]).item(), 0.)
+        self.assertEqual(torch.sum(y_hat).item(), 1.)
+
+        y_hat = classifier(x[0], 0)
+        self.assertEqual(y_hat[20].item(), 1.0)
+
+    def test_learns_when_half_the_colors_are_used(self):
+        embedding_size = 100
+        n_possible_colors = 20
+        x, y = self._create_sample_data(embedding_size, n_possible_colors, 10)
+        classifier = ColorClassifier(embedding_size, n_possible_colors, run_assertions=False).to(self.device)
+
+        optimizer = torch.optim.Adam(classifier.parameters())
+
+        n_used_colors = 10
+        ignored_predictions = torch.tensor([0] * n_used_colors + [1] * (n_possible_colors-n_used_colors) + [0], dtype=torch.bool) \
+                             .repeat(y.shape[0], 1) \
+                             .to(self.device)
+
+        for i in range(200):
+            optimizer.zero_grad()
+            y_hat = classifier(x, n_possible_colors).to(self.device)
+            loss = ((y_hat - y)**2).masked_fill(ignored_predictions, 0.).mean()
+            loss.backward()
+            optimizer.step()
+        
+        self.assertLess(loss.item(), 0.00001)
+
+
+
 
 
 
