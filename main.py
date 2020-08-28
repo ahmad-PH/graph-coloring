135a75900fb9748d4d5c590fb5c2ec3403159438
Ahmad Pourihosseini <seyedahmadpourihosseini@gmail.com>
2020-08-28 15:19:00 +0430

    Make GraphDatasetEager a subclass of GraphDataset and also give them the __iter__ method

--- main.py
+++ main.py
@@ -1,14 +1,18 @@
 import numpy as np
 import networkx as nx
-import os
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
 from torch.utils.data import Dataset, DataLoader
 from graph import Graph
-from matplotlib import pyplot as plt
+from heuristics import highest_colored_neighbor_heuristic
+import os
+from GAT import SGAT
 
 class GraphDataset(Dataset):
     def __init__(self, foldername):
         file_names = os.listdir(foldername)
-        counter = 0 
+        counter = 0
         while "{}.graph".format(counter) in file_names:
             counter += 1
         self.len = counter
@@ -20,22 +24,17 @@ class GraphDataset(Dataset):
     def __len__(self):
         return self.len
 
-class GraphDatasetEager(Dataset):
-    def __init__(self, foldername):
-        file_names = os.listdir(foldername)
-        counter = 0 
-        while "{}.graph".format(counter) in file_names:
-            counter += 1
-        self.len = counter
-        self.foldername = foldername
+    def __iter__(self):
+        return iter(self.__getitem__(i) for i in range(self.len))
 
+class GraphDatasetEager(GraphDataset):
+    def __init__(self, foldername):
+        super().__init__(foldername)
         self.graphs = [Graph.load('{}/{}.graph'.format(foldername, i)) for i in range(self.len)]
 
     def __getitem__(self, i):
         return self.graphs[i]
 
-    def __len__(self):
-        return self.len
 
 from heuristics import *
 from test import *
