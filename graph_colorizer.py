49f96d3e9ba4054d58b2e51291c364a93a93c95a
Ahmad Pourihosseini <seyedahmadpourihosseini@gmail.com>
2020-09-13 01:03:44 +0430

    Add two losses to GraphColorizer (direct/reinforce), also remove SGAT from it and replace with GraphSingularAttentionLayer

--- graph_colorizer.py
+++ graph_colorizer.py
@@ -1,71 +1,113 @@
-
 import numpy as np
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
 from graph import Graph
-from GAT import SGAT
+from GAT import GraphSingularAttentionLayer
 from heuristics import highest_colored_neighbor_heuristic
 
-# first trial: don't mask neighborhood colors
 class GraphColorizer(nn.Module):
-    def __init__(self, dropout, alpha, nheads):
+    def __init__(self, dropout=0.6, alpha=0.2, device="cpu", loss_type="reinforce"):
         super().__init__()
         self.embedding_size = 512
         self.n_possible_colors = 256
-        self.sgat = SGAT(self.embedding_size + self.n_possible_colors, self.embedding_size, self.embedding_size, dropout, alpha, nheads) 
-        self.color_classifier = ColorClassifier()
+        if loss_type not in ["reinforce", "direct"]:
+            raise ValueError("unrecognized `loss` value at GraphColorizer: {}".format(loss))
+        self.loss_type = loss_type
+        self.add_module("attn", 
+            GraphSingularAttentionLayer(self.embedding_size + self.n_possible_colors, self.embedding_size, dropout, alpha) 
+        )
+        # self.sgat = SGAT(self.embedding_size + self.n_possible_colors, self.embedding_size, self.embedding_size, dropout, alpha, nheads) 
+        self.add_module("color_classifier", 
+            ColorClassifier(self.embedding_size, self.n_possible_colors)
+        )
+        self.device = device
+        self.to(device)
 
-    def forward(self, graph: Graph):
+    def forward(self, graph: Graph, baseline=None):
         # initializations
         colors = np.array([-1] * graph.n_vertices)
-        one_hot_colors = torch.zeros(graph.n_vertices, self.n_possible_colors)
-        embeddings = torch.zeros(graph.n_vertices, self.embedding_size)
+        one_hot_colors = torch.zeros(graph.n_vertices, self.n_possible_colors).to(self.device)
+        embeddings = torch.zeros(graph.n_vertices, self.embedding_size).to(self.device)
         n_used_colors = 0
         vertex_order = highest_colored_neighbor_heuristic(graph.adj_list)
+        if self.loss_type == "direct":
+            loss = torch.tensor(0.).to(self.device)
+        elif self.loss_type == "reinforce":
+            partial_prob = torch.tensor(1.).to(self.device)
 
         # the first vertex is processed separately:
         self._assign_color(0, vertex_order[0], colors, one_hot_colors)
+        n_used_colors = 1
         for neighbor in graph.adj_list[vertex_order[0]]:
             self._update_vertex_embedding(neighbor, graph, embeddings, one_hot_colors)
 
-        for vertex in vertex_order[1:]: # first vertex was processed before
-            self._update_vertex_embedding(vertex, graph, embeddings, one_hot_colors)
+        # print('vertex order:', vertex_order)
+        # print('after initial assign:')
+        # print(colors)
+        # print(one_hot_colors)
+        # print(embeddings)
+        # print(n_used_colors)
 
-            adjacent_colors = self._get_adjacent_colors(vertex, graph, colors)
-            classifier_out = self.color_classifier(embeddings[vertex], adjacent_colors, n_used_colors)
+        for vertex in vertex_order[1:]: # first vertex was processed before
+            # print('vertex {}'.format(vertex))
+
+            self._update_vertex_embedding(vertex, graph, embeddings, one_hot_colors)
+            if self.loss_type == "direct":
+                adjacent_colors = None
+            elif self.loss_type == "reinforce":
+                adjacent_colors = self._get_adjacent_colors(vertex, graph, colors)
+            classifier_out = self.color_classifier(embeddings[vertex].clone(), n_used_colors, adjacent_colors)
 
             # use output to determine next color (decode it)
-            max_color_prob = torch.max(classifier_out)
-            max_color_index = torch.argmax(classifier_out).item()
+            max_color_index = int(torch.argmax(classifier_out).item())
             if max_color_index == self.n_possible_colors:
                 chosen_color = n_used_colors
                 n_used_colors += 1
             else:
                 chosen_color = max_color_index
-
             self._assign_color(chosen_color, vertex, colors, one_hot_colors)
+
+            # print('classifier out:', classifier_out[[0,1,2,3,self.n_possible_colors]].data)
+            if self.loss_type == "direct":
+                loss_p = self._compute_color_classifier_loss(classifier_out, adjacent_colors)
+                loss += loss_p
+            elif self.loss_type == "reinforce":
+                partial_prob *= torch.max(classifier_out)
 
             for neighbor in graph.adj_list[vertex]:
                 self._update_vertex_embedding(neighbor, graph, embeddings, one_hot_colors)
 
-            # compute a loss somehow and backprop it
-            
+        if self.loss_type == "reinforce":
+            if baseline is None: raise ValueError('baseline can not be None if loss type is `reinforcement`.')
+            print('n_used: {}, partial_prob: {}'.format(n_used_colors, partial_prob))
+            loss = (n_used_colors - baseline) * partial_prob            
 
-        # print(vertex_order)
-        # print(embeddings)
+        return colors, loss
 
     def _update_vertex_embedding(self, vertex, graph, embeddings, one_hot_colors):
-        neighbors = vertex + graph.adj_list[vertex] # because the adj_list is not self-inclusive
-        sgat_input = torch.cat([embeddings[neighbors], one_hot_colors[neighbors]], dim=1)
-        embeddings[vertex] = self.sgat(sgat_input, 0)  # 0, because we put the vertex itself first in list.
+        cloned_neighbor_embeddings = torch.cat([embeddings[vertex].unsqueeze(0).clone(), embeddings[graph.adj_list[vertex]]], dim=0)
+        cloned_neighbor_one_hot_colors = torch.cat([one_hot_colors[vertex].unsqueeze(0).clone(), one_hot_colors[graph.adj_list[vertex]]], dim=0)
+        attn_input = torch.cat([cloned_neighbor_embeddings, cloned_neighbor_one_hot_colors], dim=1)
+        embeddings[vertex] = self.attn(attn_input, 0)  # 0, because we put the vertex itself first in list.
+
+    def _compute_color_classifier_loss(self, classifier_out, adj_colors, epsilon = 1e-15):
+        max_color_prob = torch.max(classifier_out)
+        max_color_index = int(torch.argmax(classifier_out).item())
+        if max_color_index in adj_colors:
+            return 5. * - torch.log(1. - max_color_prob + epsilon)
+        elif max_color_index == self.n_possible_colors:
+            return - torch.log(1. - max_color_prob + epsilon)
+        else:
+            return 0.
 
     def _get_adjacent_colors(self, vertex, graph, colors):
-        return list(set(colors[graph.adj_list[vertex]]))
+        # "-1" is removed from the answer because -1 indicates "no color yet"
+        return list(set(colors[graph.adj_list[vertex]]).difference([-1]))
 
     def _assign_color(self, color, vertex, colors, one_hot_colors):
         colors[vertex] = color
-        one_hot_colors[vertex] = nn.functional.one_hot(torch.tensor(color), num_classes = self.n_possible_colors)
+        one_hot_colors[vertex] = F.one_hot(torch.tensor(color), num_classes = self.n_possible_colors)
 
 class ColorClassifier(nn.Module):
     def __init__(self, embedding_size, n_possible_colors, run_assertions = True):
