b7250e9af3253d0999e673269a61c9c6166ba353
Ahmad Pourihosseini <seyedahmadpourihosseini@gmail.com>
2020-09-13 14:53:28 +0430

    Move GraphDataset from main to graph_dataset.py

--- main.py
+++ main.py
@@ -8,32 +8,6 @@ from graph import Graph
 import os
 from graph_colorizer import GraphColorizer
 
-class GraphDataset(Dataset):
-    def __init__(self, foldername):
-        file_names = os.listdir(foldername)
-        counter = 0
-        while "{}.graph".format(counter) in file_names:
-            counter += 1
-        self.len = counter
-        self.foldername = foldername
-
-    def __getitem__(self, i):
-        return Graph.load('{}/{}.graph'.format(self.foldername, i))
-
-    def __len__(self):
-        return self.len
-
-    def __iter__(self):
-        return iter(self.__getitem__(i) for i in range(self.len))
-
-class GraphDatasetEager(GraphDataset):
-    def __init__(self, foldername):
-        super().__init__(foldername)
-        self.graphs = [Graph.load('{}/{}.graph'.format(foldername, i)) for i in range(self.len)]
-
-    def __getitem__(self, i):
-        return self.graphs[i]
-
 
 # ============================== Testing Region ==============================
 from matplotlib import pyplot as plt
