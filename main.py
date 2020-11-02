bc1128ce98ed2652df65247784610fde3b76de1b
Ahmad Pourihosseini <seyedahmadpourihosseini@gmail.com>
2020-11-02 22:38:04 +0330

    Separate the two runnings modes of main.py into two functions

--- main.py
+++ main.py
@@ -18,10 +18,10 @@ from networkx.algorithms import approximation
 from networkx.algorithms.community.kclique import k_clique_communities
 from networkx.algorithms.coloring.greedy_coloring import greedy_color
 from graph_dataset import GraphDataset, GraphDatasetEager
-from test import graph1, graph2
 from dataset_generators import generate_erdos_renyi, generate_watts_strogatz
-from graph_colorizer import GraphColorizer
-from utility import kneser_graph
+from utility import generate_kneser_graph, generate_queens_graph
+
+from graph_colorizer3 import GraphColorizer
 
 
 # for k in range(3,10):
@@ -36,62 +36,243 @@ from utility import kneser_graph
 # import sys; sys.exit(0)
 
 
-# generate_erdos_renyi('../data/erdos_renyi_500', 20, 5, 5, 500, 0.5)
+# generate_erdos_renyi('../data/erdos_renyi_200', 20, 5, 5, 200, 0.5)
+# generate_watts_strogatz('../data/watts_strogatz_100', 20, 5, 5, 100)
 
 
+def test_on_dataset():
+    # torch.manual_seed(1) # produces nan
 
-if __name__=='__main__':
     device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
+    ds = GraphDatasetEager('../data/erdos_renyi_100/train')
     colorizer = GraphColorizer(loss_type="reinforce", device=device)
-    optimizer = torch.optim.Adam(colorizer.parameters(), lr=0.01)
-    # graph = Graph(graph4)
+    optimizer = torch.optim.Adam(colorizer.parameters(), lr=0.0001) #0.0005)
 
-    import random
-    random.seed(0)
-    np.random.seed(0)
-    nx_g = nx.erdos_renyi_graph(200, 0.3)
-    # nx_g = nx.erdos_renyi_graph(1000, 0.5)
-    graph = Graph.from_networkx_graph(nx_g)
+    approx_answers = []
+    for i in range(len(ds)):
+        color = greedy_color(ds[i].get_nx_graph(), strategy="DSATUR")
+        approx_answers.append(len(set(color.values())))
 
-    color = greedy_color(graph.get_nx_graph())
-    print('approx:', len(set(color.values()))) 
-    # import sys; sys.exit(0)
+    baselines = [EWMAWithCorrection(0.95) for _ in range(len(ds))]
+    losses : List[List[float]] = [[] for _ in range(len(ds))]
+    n_used_list : List[List[int]] = [[] for _ in range(len(ds))]
+    final_answers = []
+    n_epochs = 20
 
-    baseline = 0.
-    decay = 0.95
-    losses = []
-    for i in range(100):
-        print("epoch: {}".format(i))
-        optimizer.zero_grad()
-        # with torch.autograd.set_detect_anomaly(True):
-        coloring, loss = colorizer.forward(graph, baseline)
-        n_used_colors = len(set(coloring))
-        baseline = decay * baseline + (1-decay) * n_used_colors
-        loss.backward()
-        optimizer.step()
-        print('loss:', loss.item())
-        print('n_used: {}, new_baseline: {}'.format(n_used_colors, baseline))
-        losses.append(loss.item())
-        print(coloring)
+    for epoch in range(n_epochs):
+        print("\nepoch: {}".format(epoch))
+        for i in range(len(ds)):
+            print('\ngraph number: {}'.format(i))
+            optimizer.zero_grad()
+            coloring, loss = colorizer.forward(ds[i], baselines[i].get_value())
+            n_used_colors = len(set(coloring))
+            n_used_list[i].append(n_used_colors)
+            baselines[i].update(n_used_colors)
+            loss.backward()
+            optimizer.step()
+            print('loss:', loss.item())
+            print('n_used: {}, new_baseline: {}'.format(n_used_colors, baselines[i].get_value()))
+            print('approx answer: {}'.format(approx_answers[i]))
+            losses[i].append(loss.item())
+            if epoch == n_epochs - 1:
+                final_answers.append(n_used_colors)
+
+    # plt.title('w norms:')
+    # plt.plot(colorizer.attn_w_norms)
+    # plt.figure()
+
+    # plt.title('a norms:')
+    # plt.plot(colorizer.attn_a_norms)
+    # plt.figure()
+
+    # plt.title('classifier norm 1:')
+    # plt.plot(colorizer.classif_norms[0])
+    # plt.figure()
+
+    # plt.title('classifier norm 2:')
+    # plt.plot(colorizer.classif_norms[1])
+    # plt.figure()
+
+    # plt.title('classifier norm 3:')
+    # plt.plot(colorizer.classif_norms[2])
+
+    # plt.show()
+
+    approx_answers = np.array(approx_answers)
+    final_answers = np.array(final_answers)
+    difference = final_answers - approx_answers
+    print('final, then greedy answers:')
+    print(final_answers)
+    print(approx_answers)
+    score = np.average(difference)
+    n_better = np.sum(difference < 0.)
+    n_worse = np.sum(difference > 0.)
+    n_equal = np.sum(difference == 0.)
+    print('score: {}, better: {}, worse: {}, equal: {}'.format(score, n_better, n_worse, n_equal))
 
 
-    plt.plot(losses)
-    plt.show()
+    # shows a plot of each graphs loss and n_used
+    for i in range(len(ds)):
+        plt.title('{}th graph losses'.format(i))
+        plt.plot(losses[i])
+        plt.figure()
+        plt.title('{}th graph n_used'.format(i))
+        plt.plot(n_used_list[i])
+        plt.show()
 
-    import sys 
+    import sys
     sys.exit()
 
 
-if __name__=='__main__':
-    gc = GraphColorizer()
-    g = Graph.from_networkx_graph(nx.erdos_renyi_graph(5, 0.5))
-    gc.forward(g)
+def test_on_single_graph():
+    device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
+    colorizer = GraphColorizer(loss_type="reinforce", device=device)
+    optimizer = torch.optim.Adam(colorizer.parameters(), lr=0.0001)
+    graph = Graph(graph1)
+
+    # graph = generate_queens_graph(5,5)
+    # graph = generate_queens_graph(6,6)
+    # graph = generate_queens_graph(7,7)
+    # graph = generate_queens_graph(8,8)
+    # graph = generate_queens_graph(8,12)
+    # graph = generate_queens_graph(11,11)
+    # graph = generate_queens_graph(13,13)
+
+    # graph = Graph.from_networkx_graph(nx.erdos_renyi_graph(100, 0.5))
+
+    # import random
+    # random.seed(0)
+    # np.random.seed(0)
+    # nx_g = nx.erdos_renyi_graph(100, 0.5)
+    # nx_g = nx.watts_strogatz_graph(200, 4, 0.5)
+    # nx_g = nx.erdos_renyi_graph(1000, 0.5)
+    # graph = Graph.from_networkx_graph(nx_g)
+
+    color = greedy_color(graph.get_nx_graph(), strategy="DSATUR")
+
+    # for strat in ['largest_first', 'random_sequential', 'smallest_last', 
+    #     'independent_set', 'connected_sequential_bfs', 'connected_sequential_dfs',
+    #     'saturation_largest_first']:
+    #     gcol = greedy_color(graph.get_nx_graph(), strategy=strat)
+    #     print('greedy with strat {}: {}'.format(strat, len(set(gcol.values())) )) 
+    # import sys; sys.exit(0)
+
+    baseline = EWMAWithCorrection(0.95)
+    losses = []
+    baselines = []
+    best_answer = float('+inf')
+    best_answer_epoch = -1
+    n_used_list = []
+
+    for i in range(100):
+        print("\n\nepoch: {}".format(i))
+        optimizer.zero_grad()
+        coloring, loss = colorizer.forward(graph, baseline.get_value())
+        n_used_colors = len(set(coloring))
+        n_used_list.append(n_used_colors)
+        baseline.update(n_used_colors)
+        loss.backward()
+
+        print('embedding grads: ', torch.mean(colorizer.embeddings.grad), torch.std(colorizer.embeddings.grad))
+        for j in range(colorizer.n_attn_layers):
+            print('attn grad:', torch.mean(colorizer.neighb_attns[j].W.grad), torch.std(colorizer.neighb_attns[j].W.grad))
+        print('classifier grad:', torch.mean(colorizer.color_classifier.fc1.weight.grad), torch.std(colorizer.color_classifier.fc1.weight.grad))
+
+        optimizer.step()
+
+        print('loss:', loss.item())
+        print('n_used: {}, new_corrected_baseline: {}'.format(n_used_colors, baseline.get_value()))
+        losses.append(loss.item())
+        baselines.append(baseline.get_value())
+        if n_used_colors < best_answer:
+            best_answer = n_used_colors
+            best_answer_epoch = i
+
+    print('best answer is {} and first happened at epoch {}'.format(best_answer, best_answer_epoch))
+    print('approx answer:', len(set(color.values()))) 
+
+    plt.title('losses')
+    plt.plot(losses)
+    plt.figure()
+    plt.title('n_used')
+    plt.plot(n_used_list)
+    plt.plot(baselines)
+    plt.show()
 
     import sys
-    sys.exit(0)
+    sys.exit()
 
 
-if __name__=='__main__':
+# # Test with single graph on embedding distance:
+# if __name__=='__main__':
+#     device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
+#     colorizer = GraphColorizer(loss_type="reinforce", device=device)
+#     optimizer = torch.optim.Adam(colorizer.parameters(), lr=0.0005)
+#     graph = Graph(graph2)
+
+#     # graph = generate_queens_graph(5,5)
+#     # graph = generate_queens_graph(6,6)
+#     # graph = generate_queens_graph(7,7)
+#     # graph = generate_queens_graph(8,8)
+#     # graph = generate_queens_graph(8,12)
+#     # graph = generate_queens_graph(11,11)
+#     # graph = generate_queens_graph(13,13)
+
+#     # graph = Graph.from_networkx_graph(nx.erdos_renyi_graph(100, 0.5))
+
+#     # import random
+#     # random.seed(0)
+#     # np.random.seed(0)
+#     # nx_g = nx.erdos_renyi_graph(100, 0.5)
+#     # nx_g = nx.watts_strogatz_graph(200, 4, 0.5)
+#     # nx_g = nx.erdos_renyi_graph(1000, 0.5)
+#     # graph = Graph.from_networkx_graph(nx_g)
+
+#     # color = greedy_color(graph.get_nx_graph(), strategy="DSATUR")
+
+#     # for strat in ['largest_first', 'random_sequential', 'smallest_last', 
+#     #     'independent_set', 'connected_sequential_bfs', 'connected_sequential_dfs',
+#     #     'saturation_largest_first']:
+#     #     gcol = greedy_color(graph.get_nx_graph(), strategy=strat)
+#     #     print('greedy with strat {}: {}'.format(strat, len(set(gcol.values())) )) 
+#     # import sys; sys.exit(0)
+
+#     # baseline = EWMA(0.95)
+#     losses = []
+#     # best_answer = float('+inf')
+#     # best_answer_epoch = -1
+#     # n_used_list = []
+#     for i in range(100):
+#         print("\n\nepoch: {}".format(i))
+#         optimizer.zero_grad()
+#         # with torch.autograd.set_detect_anomaly(True):
+#         _, loss = colorizer.forward(graph, 0.)
+#         # n_used_colors = len(set(coloring))
+#         # n_used_list.append(n_used_colors)
+#         # baseline.update(n_used_colors)
+#         loss.backward()
+#         optimizer.step()
+#         print('loss:', loss.item())
+#         # print('n_used: {}, new_corrected_baseline: {}'.format(n_used_colors, baseline.get_value()))
+#         losses.append(loss.item())
+#         # print(coloring)
+#         # if n_used_colors < best_answer:
+#             # best_answer = n_used_colors
+#             # best_answer_epoch = i
+
+#     # print('best answer is {} and first happened at epoch {}'.format(best_answer, best_answer_epoch))
+#     # print('approx answer:', len(set(color.values()))) 
+
+#     plt.title('losses')
+#     plt.plot(losses)
+#     # plt.figure()
+#     # plt.title('n_used')
+#     # plt.plot(n_used_list)
+#     plt.show()
+
+
+
+# if __name__=='__main__':
     # avg = run_heuristic_on_dataset(ordered_heuristic, GraphDataset('../data/erdos_renyi_1K/train'))
     # avg = run_heuristic_on_dataset(ordered_heuristic, GraphDataset('../data/watts_strogatz_1K/train'))
 
@@ -99,7 +280,7 @@ if __name__=='__main__':
 
     # nx_g = nx.watts_strogatz_graph(1000, 4, 0.5)
 
-    nx_g = nx.erdos_renyi_graph(1000, 0.5)
+    # nx_g = nx.erdos_renyi_graph(1000, 0.5)
     # g = Graph.from_networkx_graph(nx_g)
     # n_edge = n_edges(g.adj_list)
 
@@ -108,12 +289,11 @@ if __name__=='__main__':
 
     # G = Graph.from_networkx_graph(nx_g)
     # a = approximation.max_clique(nx_g)
-    color = greedy_color(nx_g)
+    # color = greedy_color(nx_g)
     # a = list(k_clique_communities(nx_g, 3))
-    print(len(set(color.values()))) 
+    # print(len(set(color.values()))) 
     # _, nc = colorize_using_heuristic(G.adj_list, unordered_heuristic)
     # print(nc)
-
 
 
     # ds = GraphDataset('../data/watts_strogatz_1K/train')
@@ -124,5 +304,9 @@ if __name__=='__main__':
     # valid_dl = DataLoader(GraphDatasetEager('../data/erdos_renyi_10/valid'))
     # test_dl = DataLoader(GraphDatasetEager('../data/erdos_renyi_10/test'))
 
-    # generate_erdos_renyi('erdos_renyi_', 400, 20, 20, 1000) 
+    # generate_erdos_renyi('../data/erdos_renyi_100', 20, 5, 5, 100) 
     # generate_watts_strogatz('watts_strogatz', 400, 20, 20, 1000)
+
+if __name__=="__main__":
+    # test_on_dataset()
+    test_on_single_graph()
