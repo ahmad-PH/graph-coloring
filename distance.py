a73cf219ccfa6b63335c97fedbb73a46b03ae973
Ahmad Pourihosseini <seyedahmadpourihosseini@gmail.com>
2020-12-11 23:42:36 +0330

    Change distance.py to use the reciprocal of distance to neighbors

--- distance.py
+++ distance.py
@@ -6,7 +6,11 @@ from utility import adj_list_to_matrix, generate_queens_graph
 from matplotlib import pyplot as plt
 from test import *
 import numpy as np
+from typing import List
 
+neighborhood_losses : List[float] = []
+compactness_losses : List[float] = []
+losses : List[float] = []
 
 def learn_embeddings(graph, embedding_dim = 2):
     device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
@@ -20,7 +24,7 @@ def learn_embeddings(graph, embedding_dim = 2):
 
     optimizer = torch.optim.Adam([embeddings], lr=0.1)
 
-    for i in range(400):
+    for i in range(200):
         print('epoch: {}'.format(i))
 
         optimizer.zero_grad()
@@ -37,17 +41,12 @@ def learn_embeddings(graph, embedding_dim = 2):
 
         # embedding_loss = torch.sum(all_distances * similarity_matrix)
 
-        neighborhood_loss = - torch.sum(all_distances * adj_matrix.float() / torch.sum(adj_matrix, dim=1, keepdim=True))
+        # neighborhood_loss = - torch.sum(all_distances * adj_matrix.float() / torch.sum(adj_matrix, dim=1, keepdim=True))
+        inverse_distances = 1. / (all_distances + 1e-10)
+        neighborhood_loss =  torch.sum(inverse_distances * adj_matrix.float() / torch.sum(adj_matrix, dim=1, keepdim=True))
 
         center = torch.mean(embeddings, dim=0)
         center_repeated = center.unsqueeze(0).expand(N, -1)
-
-        print('\nall_distances:')
-        print(all_distances)
-        # print('adj matrix:')
-        # print(adj_matrix)
-        print('\n')
-
         compactness_loss = torch.sum(torch.norm(embeddings - center_repeated, dim=1) ** 2)
 
         # with torch.no_grad():
@@ -66,14 +65,18 @@ def learn_embeddings(graph, embedding_dim = 2):
 
         # print('embedding_loss: {}, triplet_loss: {}'.format(embedding_loss, triplet_loss))
 
-        loss = neighborhood_loss + 0.1 * compactness_loss
-        print('neighb loss: {}, compactness loss: {}, loss: {}'.format(
-            neighborhood_loss, compactness_loss, loss))
+        _lambda = 0.01
+        loss = neighborhood_loss + _lambda * compactness_loss
+        neighborhood_losses.append(neighborhood_loss)
+        compactness_losses.append(_lambda * compactness_loss)
+        losses.append(loss)
+        # print('neighb loss: {}, compactness loss: {}, loss: {}'.format(
+            # neighborhood_loss, compactness_loss, loss))
 
         loss.backward()
         optimizer.step()
 
-        print('gradient:', embeddings.grad)
+        # print('gradient:', embeddings.grad)
 
     return embeddings
 
@@ -90,45 +93,52 @@ emb_test_graph2 = [
     [0,1]
 ]
 
-
 # graph = Graph(bipartite_10_vertices)
-graph = generate_queens_graph(13, 13)
+graph = generate_queens_graph(8, 8)
 
 # seed = np.random.randint(0, 1000000)
 # seed = 348266 # peterson 1: 
-seed = 266412
+# seed = 266412
+seed = 763012
 torch.random.manual_seed(seed)
 
 embeddings = learn_embeddings(graph, 5)
 embeddings = embeddings.detach().cpu()
-print('adj list:')
-for i in range(len(graph.adj_list)):
-    print('{}: {}'.format(i, graph.adj_list[i]))
+# print('adj list:')
+# for i in range(len(graph.adj_list)):
+#     print('{}: {}'.format(i, graph.adj_list[i]))
 
-print('embeddings')
-for i in range(embeddings.shape[0]):
-    print('{}: {}'.format(i, embeddings[i].data))
-print(embeddings.shape)
+# print('embeddings')
+# for i in range(embeddings.shape[0]):
+#     print('{}: {}'.format(i, embeddings[i].data))
+# print(embeddings.shape)
 
 print('seed is: ', seed)
 
-torch.save(embeddings, 'q13_13.pt')
+# torch.save(embeddings, 'q13_13.pt')
 
-# # plot the result:
-# x = embeddings[:, 0].squeeze().numpy()
-# y = embeddings[:, 1].squeeze().numpy()
+# plot the result:
+x = embeddings[:, 0].squeeze().numpy()
+y = embeddings[:, 1].squeeze().numpy()
 
-# for i in range(10):
-#     selected_node = i
-#     neighbors = graph.adj_list[selected_node]
-#     non_neighbors = list(set(range(graph.n_vertices)).difference(neighbors).difference([selected_node]))
-#     colors = [''] * graph.n_vertices
-#     for neighbor in neighbors: colors[neighbor] = 'r'
-#     for non_neighbor in non_neighbors: colors[non_neighbor] = 'b'
-#     colors[selected_node] = 'g'
+plt.plot(neighborhood_losses, label='neighborhood')
+plt.plot(compactness_losses, label='compactness')
+plt.plot(losses, label='overall')
+plt.legend()
+plt.show()
+plt.figure()
 
-#     # plt.scatter(x, y, c=['r','r','r','r','r', 'b','b','b','b','b'])
-#     plt.scatter(x, y, c=colors)
-#     plt.figure()
+def highlight_neighborhood(node, graph):
+    neighbors = graph.adj_list[node]
+    non_neighbors = list(set(range(graph.n_vertices)).difference(neighbors).difference([node]))
+    colors = [''] * graph.n_vertices
+    for neighbor in neighbors: colors[neighbor] = 'r'
+    for non_neighbor in non_neighbors: colors[non_neighbor] = 'b'
+    colors[node] = 'g'
+    return colors
 
-# plt.show()
+for i in range(10):
+    plt.scatter(x, y, c=highlight_neighborhood(i, graph))
+    plt.figure()
+
+plt.show()
